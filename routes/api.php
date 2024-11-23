861d08eb8b2aebf493338e8a8dca646a96384420
M Iqbal Alamsyah S <ialamsyah92@gmail.com>
2024-11-23 18:22:10 +0700

    feat(api-auth): add api for authentication

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/users', function () {
+    return User::role('user')->get();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
